541e415b1e8c57c5ee7e7981f9d8c7b0e4b15cc2
bala-amypo <bala@amypo.in>
2025-12-23 17:25:23 +0000

    User commit

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,1 +1,42 @@
-Template
+package com.example.demo.service;
+
+import com.example.demo.entity.Student;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Transactional
+    public Student addStudent(Student student) {
+
+        boolean emailExists =
+                studentRepository.findByEmail(student.getEmail()).isPresent();
+
+        if (emailExists) {
+            throw new RuntimeException("Student email exists");
+        }
+
+        return studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById(Long id) {
+        return studentRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Student not found"));
+    }
+}
