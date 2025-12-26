009b8965e15d6fe30b765d1b8b73150c13dfe2c5
bala-amypo <bala@amypo.in>
2025-12-26 17:27:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -49,7 +49,8 @@ public class AuthController {
                     "email", user.getEmail(),
                     "role", user.getRole()
                 );
-                String token = jwtUtil.generateToken(claims, user.getEmail());
+                String token = jwtUtil.generateToken(user.getEmail());
+
                 return ResponseEntity.ok(new AuthResponse(token));
             }
             return ResponseEntity.status(401).build();
